126f4be504f73285c6b8b05be7834c36de85bcb5
Opilane TTHK <opilane@tthk.ee>
2020-10-19 15:01:32 +0300

    first commit

---
+++ YearValitator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YearValitator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Yearvalidator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Yearvalidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
